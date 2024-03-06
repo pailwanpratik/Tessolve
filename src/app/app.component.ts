3a4c415c6577b41a19499380d1ad667cdbffcb19
Pratik Pailwan <11835@Tessolveindia.com>
2024-03-06 16:01:36 +0530

    initial version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { IDropdownSettings } from 'ng-multiselect-dropdown';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'fullapp';
+export class AppComponent implements OnInit {
+  dropdownList: { item_id: number, item_text: string }[] = []; // Explicitly typed
+  selectedItems: { item_id: number, item_text: string }[] = []; // Explicitly typed
+  dropdownSettings: IDropdownSettings = {}; // Provide an initializer
+  widgets = ['w1', 'w2', 'w3', 'w4', 'w5', 'w6', 'w7'];
+  flowChart: string[] = [];
+
+  ngOnInit() {
+    this.dropdownList = [
+      { item_id: 1, item_text: 'System 1' },
+      { item_id: 2, item_text: 'System 2' },
+      { item_id: 3, item_text: 'System 3' },
+      { item_id: 4, item_text: 'System 4' },
+      { item_id: 5, item_text: 'System 5' },
+      { item_id: 6, item_text: 'System 6' },
+      { item_id: 7, item_text: 'System 7' }
+    ];
+    this.selectedItems = [
+    ];
+    this.dropdownSettings = {
+      singleSelection: false,
+      idField: 'item_id',
+      textField: 'item_text',
+      selectAllText: 'Select All',
+      unSelectAllText: 'UnSelect All',
+      itemsShowLimit: 3,
+      allowSearchFilter: true
+    };
+  }
+
+  onItemSelect(item: any) {
+    console.log(item);
+  }
+  onSelectAll(items: any) {
+    console.log(items);
+  }
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
+    console.log(this.flowChart);
+    console.log(this.widgets);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgMultiSelectDropDownModule } from 'ng-multiselect-dropdown';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DragDropModule } from '@angular/cdk/drag-drop'
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    DragDropModule,
+    NgMultiSelectDropDownModule
   ],
   providers: [],
   bootstrap: [AppComponent]
